5ad8f6addffca255349cf5d1a66126f552bf6d27
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-15 04:11:11 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,8 @@
 require 'pry'
-
 def alphabetize(arr)
   alphabets = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|word| word.each_char.map {|char| alphabets.index(char)}}
+
+  arr.sort_by do |word|
+    word.each_char.map {|char| alphabets.index(char)}
+  end
 end

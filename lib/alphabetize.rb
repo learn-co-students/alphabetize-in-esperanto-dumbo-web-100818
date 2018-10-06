33cf8babd86fe692c50df5fa482410c3497ca413
Carson Rey <c.rey494@gmail.com>
2018-10-06 15:56:48 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |string|
+    string.split("").collect do |letter|
+        ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
+
+# don't need to iterate down to 'letter' from each word in the string. '.split("")' takes care of splitting each word into letters.
+
+#'.split' on its own will default to seperating words in a sentence, but given an argument of '("")' it will split each element(word) with no space in between as well as the space between words.
